4bab5ed82306bcd4ac024dd63b9a948c86c70f45
Feyza <feyzabakir51@gmail.com>
2024-07-25 08:18:33 +0300

    Model added and path settings made

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.published.all()  # Özel manager kullanarak sadece yayınlanan postları alın
+    return render(
+        request,
+        'blog/post_list.html',
+        {'posts': posts}
+    )
+
+def post_detail(request, id):
+    post = get_object_or_404(
+        Post, 
+        id=id,
+        status=Post.Status.PUBLISHED  # Sadece yayınlanan postları alın
+    )
+    return render(
+        request,
+        'blog/post_detail.html',
+        {'post': post}
+    )
