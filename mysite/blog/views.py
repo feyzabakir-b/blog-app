df02dd48e858fc278d5d866cd0c4107a304f77d0
Feyza <feyzabakir51@gmail.com>
2024-07-26 11:55:48 +0300

    feat: added pagination

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,19 +1,35 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from .models import Post
+from django.views.generic import ListView
 
-def post_list(request):
-    posts = Post.published.all()  # Özel manager kullanarak sadece yayınlanan postları alın
-    return render(
-        request,
-        'blog/post/list.html',
-        {'posts': posts}
-    )
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
-def post_detail(request, id):
+# def post_list(request):
+#     post_list = Post.published.all()  
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     return render(
+#         request,
+#         'blog/post/list.html',
+#         {'posts': posts}
+#     )
+
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post, 
-        id=id,
-        status=Post.Status.PUBLISHED  # Sadece yayınlanan postları alın
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__month=month,
+        publish__day=day
     )
     return render(
         request,
